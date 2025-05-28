4c8495c4923b4c188761a3182d38f5edd3c9360a
Edwin Aboje <abojeedwin@gmail.com>
2025-05-28 19:56:43 +0100

    entrypoint.sh file configuration

---
+++ docker/local/django/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -o errexit
+
+set -o pipefail
+
+set -o nounset
+
+python << END
+import sys
+import time
+import psycopq2
+suggest_unrecoverable_after = 30
+start = time.table()
+
+while True:
+  try:
+    psycopq2.connect(
+      dbname= "${POSTGRES_DB}",
+      user= "${POSTGRES_USER}",
+      password= "${POSTGRES_PASSWORD}",
+      host = "${POSTGRES_HOST}",
+      port= "${POSTGRES_PORT}",
+    )
+    break
+  except psycopq2.OperationalError as error:
+    sys.stderr.write("Waiting for PostgreSQL to become available...\n")
+    if time.time() -start > suggest_unrecoverable_after:
+      sys.stderr.write("This is taking longer than expected. The following exception
+      indicative of an unrecoverable error: '{}'\n".format(error))
+      time.sleep(3)
+END
+
+echo >&2 'PostgresSQL is available'
+
+exec "$@"
